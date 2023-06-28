fab1bed5e764f9db71fe9d2e5db20c71ef49b354
ishneeeeeet <ishneet100@gmail.com>
2023-06-27 23:03:18 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
-
 import Header from "./components/Header/Header";
 import Body from "./components/Body/Body";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Body />
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/body" element={<Body/>}/>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -8,7 +8,7 @@ import Resume from "../Resume/Resume";
 import styles from "./Body.module.css";
 
 function Body() {
-  const colors = ["#239ce2", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
+  const colors = ["#F50157", "#48bb78", "#0bc5ea", "#a0aec0", "#ed8936"];
   const sections = {
     basicInfo: "Basic Info",
     workExp: "Work Experience",
@@ -61,7 +61,7 @@ function Body() {
 
   return (
     <div className={styles.container}>
-      <p className={styles.heading}>Resume Builder</p>
+      <p className={styles.heading}></p>
       <div className={styles.toolbar}>
         <div className={styles.colors}>
           {colors.map((item) => (
